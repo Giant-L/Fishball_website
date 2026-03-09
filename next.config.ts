7810e6bdc876c1ebedc1b137f38ecd8cadbd11bb
烨Sapphire <13534864+ye-sapphire@user.noreply.gitee.com>
2026-03-09 20:47:43 +0800

    fix: 移除不兼容的 eslint 配置项

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 忽略 TypeScript 报错，强行打包
+  // 仅忽略 TypeScript 报错，强行打包
   typescript: {
     ignoreBuildErrors: true,
-  },
-  // 忽略 ESLint 报错，强行打包
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
